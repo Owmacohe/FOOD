9d860213eebc15857e2263c9118ed23adfab2e21
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-09-09 16:04:35 -0400

    Initial framework for deliveries and islands

---
+++ Assets/Scripts/Islands/IslandManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class IslandManager : MonoBehaviour
+{
+    List<Island> islands;
+    
+    Island deliveryTarget;
+    float deliveryStartTime;
+    
+    float funds; // TODO: this may want to be moved to another script
+    int strikes;
+
+    Island CreateIsland(Vector3 pos)
+    {
+        Island temp = new Island(
+            pos,
+            Random.Range(1, 15),
+            Random.Range(0, 60) + 60 // TODO: this should be chosen based (somewhat) on current player distance to the island
+        );
+
+        islands.Add(temp);
+        
+        return temp;
+    }
+    
+    void ChooseTarget()
+    {
+        deliveryTarget = islands[Random.Range(0, islands.Count)]; // TODO: this should be more likely to choose a closer target than a far one (or follow a preset path)
+        deliveryStartTime = Time.time;
+    }
+
+    void CompleteDelivery()
+    {
+        funds += deliveryTarget.DeliveryTime - (Time.time - deliveryStartTime); // TODO: this may want to be tweaked still
+        islands.Remove(deliveryTarget);
+        
+        deliveryTarget = null;
+        deliveryStartTime = 0;
+        
+        // TODO: add win condition?
+        
+        Reset();
+    }
+
+    void FailDelivery()
+    {
+        strikes++;
+
+        if (strikes >= 3)
+        {
+            // TODO: lose state
+        }
+        else
+        {
+            Reset();   
+        }
+    }
+
+    void Reset()
+    {
+        deliveryTarget = null;
+        deliveryStartTime = 0;
+        
+        ChooseTarget(); // TODO: this may want to be delayed
+    }
+}
--- Assets/Scripts/Movement/DirectionController.cs
+++ Assets/Scripts/Movement/DirectionController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
 
 public class DirectionController : MonoBehaviour
@@ -18,7 +18,8 @@ public class DirectionController : MonoBehaviour
 
     Rigidbody rb;
     
-    bool isRotating;
+    bool isRotating, isArriving;
+    int rotationDirection;
     Vector3 direction;
 
     float startTime;
@@ -28,13 +29,22 @@ public class DirectionController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         
         isRotating = true;
+        rotationDirection = 1;
+    }
+
+    void Update()
+    {
+        if (transform.position.y != 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
     }
 
     void FixedUpdate()
     {
         if (isRotating)
         {
-            arrow.RotateAround(transform.position, Vector3.up, rotationSpeed);
+            arrow.RotateAround(transform.position, Vector3.up, rotationSpeed * rotationDirection);
 
             if (!direction.Equals(Vector3.zero))
             {
@@ -46,22 +56,76 @@ public class DirectionController : MonoBehaviour
 
     public void SetDirection()
     {
-        isRotating = false;
-        startTime = Time.time;
+        SetDirection((arrow.position - transform.position).normalized);
+    }
+    
+    void SetDirection(Vector3 dir)
+    {
+        if (!isArriving)
+        {
+            isRotating = false;
+            startTime = Time.time;
         
-        direction = (arrow.position - transform.position).normalized;
+            direction = dir;
+            rotationDirection *= -1;   
+        }
     }
 
     public void SetForce()
     {
-        isRotating = true;
-        forceSpeed = (Time.time - startTime) * forceSpeedModifier;
-
-        if (forceSpeed > forceSpeedMax)
+        if (!isArriving)
         {
-            forceSpeed = forceSpeedMax;
-        }
+            isRotating = true;
+            forceSpeed = (Time.time - startTime) * forceSpeedModifier;
+
+            if (forceSpeed > forceSpeedMax)
+            {
+                forceSpeed = forceSpeedMax;
+            }
         
-        rb.velocity += direction * forceSpeed;
+            rb.velocity += direction * forceSpeed;   
+        }
+    }
+
+    IEnumerator ArriveGently(float minimumSpeed)
+    {
+        for (float i = 0; i < 1; i += 0.1f)
+        {
+            rb.velocity /= 1.3f;
+
+            if (rb.velocity.magnitude < (direction * minimumSpeed).magnitude)
+            {
+                rb.velocity = rb.velocity.normalized * minimumSpeed;
+                break;
+            }
+            
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            isRotating = false;
+            isArriving = true;
+
+            Vector3 dir = (other.transform.position - transform.position).normalized;
+            SetDirection(dir);
+            
+            rb.velocity = dir * forceSpeed;
+            
+            StartCoroutine(ArriveGently(3));
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            isRotating = true;
+            isArriving = false;
+            rb.velocity = Vector3.zero;
+        }
     }
 }
