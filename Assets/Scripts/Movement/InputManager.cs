45259a0a8c1414675d3bd3cd55e102ee74346d46
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-09-10 11:13:23 -0400

    Started work on an Options class

--- Assets/Scripts/Movement/InputManager.cs
+++ Assets/Scripts/Movement/InputManager.cs
@@ -6,14 +6,27 @@ public class InputManager : MonoBehaviour
     DirectionController direction;
 
     bool isDown;
+    string currentKeyName;
 
     void Start()
     {
         direction = GetComponent<DirectionController>();
     }
+    
+    void OnGUI()
+    {
+        Event e = Event.current;
+        
+        if (e.isKey && e.keyCode != KeyCode.None)
+        {
+            currentKeyName = e.keyCode.ToString();
+        }
+    }
 
     void OnShort()
     {
+        // TODO: check if currentKeyName equals the Option object's CurrentKey
+        
         if (!isDown)
         {
             direction.SetDirection();
